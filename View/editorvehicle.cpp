0f2656403a626f6cf529b377ed0b76cdc6610b9e
Andrea <93330631+WatEagle001@users.noreply.github.com>
2023-01-10 12:01:57 +0100

    Implementata GUI modifica veicolo

--- View/editorvehicle.cpp
+++ View/editorvehicle.cpp
@@ -3,6 +3,8 @@
 #include <QString>
 #include <iostream>
 #include <QLineEdit>
+#include <QComboBox>
+#include <QFormLayout>
 
 QLayout* editorvehicle::configureFinalLayout(){
     // Creazione Layout
@@ -15,24 +17,45 @@ QLayout* editorvehicle::configureFinalLayout(){
     return mainLayout;
 }
 
-QVBoxLayout *editorvehicle::configureEditor()
-{
-    QVBoxLayout* edit = new QVBoxLayout;
-    //QItemEditorFactory *factory = new QItemEditorFactory;
+QFormLayout *editorvehicle::configureEditor(){
+     tipoVeicolo = new QComboBox;
+     tipoAlimentazione = new QComboBox;
+        layout = new QFormLayout;
+       layout->addWidget(tipoVeicolo);
+       layout->insertRow(1,new QLabel(tr("Tipologia Veicolo")), tipoVeicolo);
+       layout->insertRow(2,new QLabel(tr("Marca")), new QLineEdit);
+       layout->insertRow(3,new QLabel(tr("Modello")), new QLineEdit);
+       layout->insertRow(4,new QLabel(tr("Targa")), new QLineEdit);
+       layout->insertRow(5,new QLabel(tr("Km Odometro")), new QLineEdit);
+       layout->insertRow(6,new QLabel(tr("Cilindrata")), new QLineEdit);
+       layout->insertRow(7,new QLabel(tr("Litri Carburante")), new QLineEdit);
+       layout->insertRow(8,new QLabel(tr("Carburante")), tipoAlimentazione);
+       layout->insertRow(9,new QLabel(tr("Manutenzione")), new QLineEdit);
+       layout->insertRow(10,new QLabel(tr("Costo Manutenzione")), new QLineEdit);
+       layout->insertRow(11,new QLabel(tr("KW Batteria")), new QLineEdit);
+       layout->insertRow(12,new QLabel(tr("Ricaricare")), new QLineEdit);
+       layout->insertRow(13,new QLabel(tr("Costo Ricarica")), new QLineEdit);
 
-        /*QItemEditorCreatorBase *colorListCreator =
-            new QStandardItemEditorCreator<ColorListEditor>();
 
-        factory->registerEditor(QMetaType::QColor, colorListCreator);
+       connect(tipoVeicolo, QOverload<int>::of(&QComboBox::currentIndexChanged),
+           [=](int index){addMoreOptions(); });
 
-        QItemEditorFactory::setDefaultFactory(factory);
-        */
-    //edit->addWidget(factory);
-    QString test("Seleziona tipologia veicolo con cui convertire il mezzo selezionato");
-    QLineEdit* test1 = new QLineEdit;
-    edit->addWidget(test1);
+    tipoVeicolo->addItem(QString("Automobile"));
+    tipoVeicolo->addItem(QString("Automobile elettrica"));
+    tipoVeicolo->addItem(QString("Automobile ibrida"));
+    tipoVeicolo->addItem(QString("Moto"));
+    tipoVeicolo->addItem(QString("Moto elettrica"));
+    tipoVeicolo->addItem(QString("Monopattino elettrico"));
 
-    return edit;
+    tipoAlimentazione->addItem(QString("Benzina"));
+    tipoAlimentazione->addItem(QString("Diesel"));
+    tipoAlimentazione->addItem(QString("Metano"));
+    tipoAlimentazione->addItem(QString("GPL"));
+    tipoAlimentazione->addItem(QString("BioDiesel"));
+    tipoAlimentazione->addItem(QString("Idrogeno"));
+
+
+    return layout;
 }
 
 QHBoxLayout *editorvehicle::configureButtons()
@@ -50,6 +73,31 @@ QHBoxLayout *editorvehicle::configureButtons()
     buttons->addWidget(clear);
 
     return buttons;
+}
+
+void editorvehicle::addMoreOptions()
+{
+
+        deleteOptions();
+
+}
+
+void editorvehicle::deleteOptions()
+{
+    if(tipoVeicolo->currentIndex() == 1
+            || tipoVeicolo->currentIndex() == 4 || tipoVeicolo->currentIndex() == 5){
+    layout->removeRow(6);
+    layout->removeRow(7);
+    layout->removeRow(8);
+    layout->removeRow(9);
+    layout->removeRow(10);
+
+    }
+    if(tipoVeicolo->currentIndex() == 0 || tipoVeicolo->currentIndex() == 2 ||tipoVeicolo->currentIndex() == 3){
+        layout->removeRow(11);
+        layout->removeRow(12);
+        layout->removeRow(13);
+    }
 }
 
 void editorvehicle::connectViewSignals() const
